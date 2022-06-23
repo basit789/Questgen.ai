36569cf4316af7de8a8462a194fec61163f61528
basit789 <93515902+basit789@users.noreply.github.com>
2022-06-22 23:32:06 -0700

    Update main.py

--- Questgen/main.py
+++ Questgen/main.py
@@ -106,7 +106,7 @@ class QGen:
     def predict_shortq(self, payload):
         inp = {
             "input_text": payload.get("input_text"),
-            "max_questions": payload.get("max_questions", 4)
+            "max_questions": payload.get("max_questions", 10)
         }
 
         text = inp['input_text']
